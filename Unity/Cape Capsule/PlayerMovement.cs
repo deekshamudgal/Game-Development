9f6e9f042bc2e1b3a4ebddfe9a5fbc770f76ac69
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2024-02-03 00:40:39 +0530

    Shortened the code and smoothened the player movement

--- Unity/Cape Capsule/PlayerMovement.cs
+++ Unity/Cape Capsule/PlayerMovement.cs
@@ -1,42 +1,38 @@
+//Is Rigidbody a kind of variable storage thingy? 
+//how are we able to store a function in it though? Is that allowed??!
+//and how is velocity a variable stored in our Rigidbody? Am I looking too much into this?
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
-        }
+        //GetAxis is much smoother than GetAxisRaw
 
-        if (Input.GetKey("up"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
-        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey("down"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
-        }
+        rb.velocity = new Vector3(horizontalInput, rb.velocity.y, verticalInput);
 
-        if (Input.GetKey("right"))
+        if(Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
-        }
+            //Vector3 expects float values
 
-        if (Input.GetKey("left"))
-        {
-            GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+            rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
         }
+       
         
     }
 }
+
