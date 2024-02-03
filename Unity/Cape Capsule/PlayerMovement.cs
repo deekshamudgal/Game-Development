dc3db3e51b48e60dcbd7ee2a55cec81b753ac240
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2024-02-03 10:38:42 +0530

    Made the movementSpeed and jumpForce descriptive

--- Unity/Cape Capsule/PlayerMovement.cs
+++ Unity/Cape Capsule/PlayerMovement.cs
@@ -8,14 +8,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Variables are always preferred when you need to make things descriptive and self-explanatory
+
     Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
     // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
+
     void Update()
     {
         //GetAxis is much smoother than GetAxisRaw
@@ -23,14 +30,18 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontalInput, rb.velocity.y, verticalInput);
+        //HELP: {the movement is relatively slower now because instead of setting the value to 0 and relying on the physics, we let the button down do it's work...}
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
         if(Input.GetButtonDown("Jump"))
         {
-            //Vector3 expects float values
+            //Vector3 expects float values, thus the f after 5
 
-            rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
+
+        //QUESTION OF THE DAY: WHAT IS EXPLICIT AND IMPLICIT? IN TERMS OF PROGRAMMING? IN TERMS OF FUNCTIONS?
        
         
     }
