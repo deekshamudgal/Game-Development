d8a0df35555521f2be612778742e20b4527f91bc
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2024-02-08 11:37:00 +0530

    Update PlayerLife.cs
    Fixed the reload bug that was not allowing the game to reload itself when the player dies by the touching the enemy.

--- Unity/Cape Capsule/PlayerLife.cs
+++ Unity/Cape Capsule/PlayerLife.cs
@@ -8,25 +8,27 @@ public class NewBehaviourScript : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Enemy Body"))
+        if (collision.gameObject.CompareTag("Enemy Body"))
         {
             Die();
         }
 
         void Die()
         {
-            GetComponent<MeshRenderer> ().enabled = false; //WOOSH, will disappear like magix~~
+            GetComponent<MeshRenderer>().enabled = false; //WOOSH, will disappear like magix~~
             GetComponent<Rigidbody>().isKinematic = true; //by enabling isKinematic in the rigid body component we disable the player to affected by the physics
-            GetComponent<PlayerMovement> ().enabled = false;
-            Invoke(nameof(ReloadLevel), 0.9f); //the time delay is 1.3 seconds because I liked the value...Because why not~
+            GetComponent<PlayerMovement>().enabled = false;
+            //why is it that when I reload my level, it's lighting doesn't change but other people's does....?
+            Invoke(nameof(ReloadLevel), 1.3f); //the time delay is 1.3 seconds because I liked the value...Because why not~
 
             //we could have also used Destroy() inbuilt function but it will also remove the Player life script. 
 
         }
-
-        void ReloadLevel()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+    }
+    //why is it that when this function was in the if statement block it did not work it's reference in Invoke but...
+    //when I got it out, it magically started working with the Invoke. What sorcery is this??
+    void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
