23b22e7c65347abda97c4641345266b628e594da
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2024-02-11 17:00:16 +0530

    Update ItemCollector.cs

--- Unity/Cape Capsule/ItemCollector.cs
+++ Unity/Cape Capsule/ItemCollector.cs
@@ -7,6 +7,7 @@ public class ItemCollector : MonoBehaviour
 {
     int coins = 0;
     [SerializeField] Text coinsText;
+    [SerializeField] AudioSource collectionSound;
     //had to use Ontrigger rather OnCollision as we will check  the IsTrigger functionality for all collectibles
     //so that we can pass through them rather than sticking onto their surface
     private void OnTriggerEnter(Collider other)
@@ -16,6 +17,8 @@ public class ItemCollector : MonoBehaviour
             Destroy(other.gameObject);
             coins++;
             coinsText.text="Coins:" + coins;
+
+        collectionSound.Play();
         }
     }
 }
